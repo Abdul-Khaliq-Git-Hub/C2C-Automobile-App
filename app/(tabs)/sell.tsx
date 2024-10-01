979fdd7da9b4a25fecb490be7f4d478fbd921dad
Abdul Khaliq <abdulkhaliqise2023@gmail.com>
2024-10-01 15:53:05 +0530

    Added additional Inputs

--- app/(tabs)/sell.tsx
+++ app/(tabs)/sell.tsx
@@ -3,17 +3,31 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useState } from "react";
 import FormField from "../../components/FormField";
 import FormFieldDropdown from "../../components/FormFieldDropdown";
+import FuelSelection from "../../components/SelectionForm";
+import TransmissionSelection from "../../components/SelectionForm";
+import ConditionSelection from "../../components/SelectionForm";
 import brandTypes from "../../constants/brandTypes";
 import truckTypes from "../../constants/truckTypes";
 
 const Sell = () => {
   const [brandPickerVisible, setBrandPickerVisible] = useState(false);
   const [truckPickerVisible, setTruckPickerVisible] = useState(false);
+  const fuelTypes = ["Petrol", "Diesel", "LPG", "CNG & Hybrids", "Electric"];
+  const conditionType = ["New", "Used"];
+  const transmissionType = ["Manual", "Automatic", "Hybrid"];
 
   const [form, SetForm] = useState({
     Brand: "",
     TruckType: "",
     Year: "",
+    Fuel: "",
+    Condition: "",
+    Transmission: "",
+    KiloMeterDriven: "",
+    Price: "",
+    SellerName: "",
+    SellerPhone: "",
+    SellerLocation: "",
   });
 
   const handleBrandChange = (value: string) => {
@@ -29,6 +43,12 @@ const Sell = () => {
     });
     setTruckPickerVisible(false); // Update the state with the selected value
   };
+
+  const handleChangeText = (value: string, name: string) => {
+    SetForm((prevData) => {
+      return { ...prevData, [name]: value };
+    });
+  };
   return (
     <SafeAreaView className="h-full">
       <ScrollView style={{ opacity: brandPickerVisible ? 0.1 : 1 }}>
@@ -38,6 +58,7 @@ const Sell = () => {
         <View className="m-2">
           <Text className="text-2xl">Include Truck Details</Text>
         </View>
+        {/* Brand Dropdown */}
         <FormFieldDropdown
           constant={brandTypes}
           title="Brand"
@@ -46,6 +67,8 @@ const Sell = () => {
           pickerVisible={brandPickerVisible}
           handleChange={handleBrandChange}
         />
+
+        {/* Truck Type Dropdown */}
         <FormFieldDropdown
           constant={truckTypes}
           title="Truck Type"
@@ -54,7 +77,84 @@ const Sell = () => {
           pickerVisible={truckPickerVisible}
           handleChange={handleTruckChange}
         />
-        <FormField title="Year" value={form.Year} otherStyles="m-2" />
+
+        {/* Year Field */}
+        <FormField
+          title="Year"
+          name="Year"
+          value={form.Year}
+          otherStyles="m-2"
+          handleChangeText={(value) => handleChangeText(value, "Year")}
+        />
+
+        {/* Kilometer Driven Field */}
+        <FormField
+          title="KM"
+          name="KiloMeterDriven"
+          value={form.KiloMeterDriven}
+          otherStyles="m-2"
+          handleChangeText={(value) =>
+            handleChangeText(value, "KiloMeterDriven")
+          }
+        />
+
+        {/* Price Field */}
+        <FormField
+          title="Price"
+          name="Price"
+          value={form.Price}
+          otherStyles="m-2"
+          handleChangeText={(value) => handleChangeText(value, "Price")}
+        />
+
+        {/* Seller Name Field */}
+        <FormField
+          title="Name"
+          name="SellerName"
+          value={form.SellerName}
+          otherStyles="m-2"
+          handleChangeText={(value) => handleChangeText(value, "SellerName")}
+        />
+
+        {/* Seller Phone Field */}
+        <FormField
+          title="Phone"
+          name="SellerPhone"
+          value={form.SellerPhone}
+          otherStyles="m-2"
+          handleChangeText={(value) => handleChangeText(value, "SellerPhone")}
+        />
+
+        {/* Seller Location Field */}
+        <FormField
+          title="Location"
+          name="SellerLocation"
+          value={form.SellerLocation}
+          otherStyles="m-2"
+          handleChangeText={(value) =>
+            handleChangeText(value, "SellerLocation")
+          }
+        />
+
+        {/* Fuel Selection */}
+        <FuelSelection
+          selection={fuelTypes}
+          name="Fuel"
+          value={form.Fuel}
+          handleChangeText={(value) => handleChangeText(value, "Fuel")}
+        />
+        <TransmissionSelection
+          selection={transmissionType}
+          name="Transmission"
+          value={form.Transmission}
+          handleChangeText={(value) => handleChangeText(value, "Transmission")}
+        />
+        <ConditionSelection
+          selection={conditionType}
+          name="Condition"
+          value={form.Condition}
+          handleChangeText={(value) => handleChangeText(value, "Condition")}
+        />
       </ScrollView>
     </SafeAreaView>
   );
