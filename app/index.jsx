30669de9df57b0f3f78e277fbb5b0065221b8242
Abdul Khaliq <abdulkhaliqise2023@gmail.com>
2024-10-14 18:40:29 +0530

    Added Kurdish Language

--- app/index.jsx
+++ app/index.jsx
@@ -13,33 +13,41 @@ export default function App() {
     i18next.changeLanguage(lng);
     router.push("/home");
   };
-
+  console.log("LanguagesPage");
   return (
-    <SafeAreaView className="bg-white h-full">
-      <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full items-center min-h-[85vh] px-4">
-          <Image
-            source={images.logo}
-            className="w-[130px] h-[84px]"
-            resizeMode="contain"
-          />
-          <View className="w-full flex-1 items-center justify-center h-full">
-            <Text className="text-xl font-pmedium">Choose Your Language</Text>
-            <TouchableOpacity
-              className="w-[200px] bg-white p-4 mt-4 border rounded-2xl border-gray-300"
-              onPress={() => changeLng("en")}
-            >
-              <Text className="text-xl">English</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              className="w-[200px] bg-white p-4 mt-4 border rounded-2xl border-gray-300"
-              onPress={() => changeLng("ar")}
-            >
-              <Text className="text-xl">عربي</Text>
-            </TouchableOpacity>
+    <>
+      <SafeAreaView className="bg-white h-full">
+        <ScrollView contentContainerStyle={{ height: "100%" }}>
+          <View className="w-full items-center min-h-[85vh] px-4">
+            <Image
+              source={images.logo}
+              className="w-[150px] h-[84px]"
+              resizeMode="contain"
+            />
+            <View className="w-full flex-1 items-center justify-center h-full">
+              <Text className="text-xl font-pmedium">Choose Your Language</Text>
+              <TouchableOpacity
+                className="w-[200px] bg-white p-4 mt-4 border rounded-2xl border-gray-300"
+                onPress={() => changeLng("en")}
+              >
+                <Text className="text-xl">English</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className="w-[200px] bg-white p-4 mt-4 border rounded-2xl border-gray-300"
+                onPress={() => changeLng("ar")}
+              >
+                <Text className="text-xl">عربي</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className="w-[200px] bg-white p-4 mt-4 border rounded-2xl border-gray-300"
+                onPress={() => changeLng("ku")}
+              >
+                <Text className="text-xl">کوردی</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 }
