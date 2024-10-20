555c96e130e3b1c4fd9b87ec164b1ff8feb506f7
Abdul Khaliq <abdulkhaliqise2023@gmail.com>
2024-10-20 18:25:40 +0530

    Added feature to save product as favourite

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,18 +1,48 @@
-import { View, Text, Image } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { images, icons } from "../constants";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { icons } from "../constants";
+import { useState, useRef, useEffect } from "react";
+import {
+  getFirestore,
+  doc,
+  updateDoc,
+  arrayRemove,
+  arrayUnion,
+} from "firebase/firestore";
+import { useUser } from "../context/UserContext";
 
 interface CarProductProps {
+  id: string;
   Brand: string;
   Engine: string;
-  Cylinder: string;
-  Miles: number;
+  Miles: string;
   Location: string;
   Price: string;
   images: string[];
 }
-
+console.log("productpage");
 export default function Product(props: CarProductProps) {
+  const renderCount = useRef(0);
+  const user = useUser();
+  const [heart, setHeart] = useState(false);
+  console.log(user);
+  useEffect(() => {
+    renderCount.current += 1;
+    console.log(`Product component has rendered ${renderCount.current} times`);
+  });
+  const handleFavourite = async () => {
+    if (user) {
+      const userDocRef = doc(getFirestore(), "users", user.uid);
+      try {
+        await updateDoc(userDocRef, {
+          favourites: heart ? arrayUnion(props.id) : arrayRemove(props.id),
+        });
+        setHeart((prevState) => !prevState);
+      } catch (error) {
+        console.error("Error updating favourites:", error);
+      }
+    }
+  };
+
   return (
     <View className="m-4 rounded-lg border border-gray-200">
       <Image
@@ -26,6 +56,17 @@ export default function Product(props: CarProductProps) {
         resizeMode="cover"
       />
 
+      <TouchableOpacity
+        style={{ position: "absolute", right: 5, top: 3 }}
+        onPress={() => handleFavourite()}
+      >
+        <Image
+          source={heart ? icons.unmarkedheart : icons.heart}
+          resizeMode="contain"
+          className="h-6 w-6"
+        />
+      </TouchableOpacity>
+
       {/* Car Details */}
       <View className="p-4 bg-white rounded-b-lg">
         <Text className="text-xl font-semibold mb-2">{props.Brand}</Text>
@@ -37,26 +78,27 @@ export default function Product(props: CarProductProps) {
             <Text className="text-sm mt-2">Engine,{props.Engine}</Text>
           </View>
 
-          {/* Cylinder */}
-          <View className="items-center">
-            <Image source={icons.cylinder} style={{ width: 35, height: 35 }} />
-            <Text className="text-sm mt-2">{props.Cylinder} Cylinder</Text>
-          </View>
-
           {/* Odometer */}
           <View className="items-center">
             <Image source={icons.odometer} style={{ width: 35, height: 35 }} />
             <Text className="text-sm mt-2">{props.Miles} Miles</Text>
           </View>
         </View>
-      </View>
 
-      {/* Location and Price */}
-      <View className="flex-row justify-between px-4 py-2 bg-gray-50 rounded-b-lg">
-        <Text className="text-sm text-gray-600">{props.Location}</Text>
-        <Text className="text-sm font-semibold text-gray-800">
-          ${props.Price}
-        </Text>
+        {/* Location and Price */}
+        <View className="flex-row justify-between px-4 py-2 bg-white-50 rounded-b-lg">
+          <View className="flex-row">
+            <Image
+              className="w-5 h-5"
+              source={icons.locationpin}
+              resizeMode="contain"
+            />
+            <Text className="text-sm text-gray-600">{props.Location}</Text>
+          </View>
+          <Text className="text-sm font-semibold text-gray-800">
+            ${props.Price}
+          </Text>
+        </View>
       </View>
     </View>
   );
