279786e1a5955c256768cc608ba9c566d1d20f80
Abdul Khaliq <abdulkhaliqise2023@gmail.com>
2024-10-24 00:09:21 +0530

    Added truck logos and types used in  search functionality

---
+++ constants/truckLogos.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    name: "Tipper Trucks",
+    logo: require("../assets/images/Tipper Trucks.png"),
+  },
+  { name: "Box Trucks", logo: require("../assets/images/Box Trucks.png") },
+  {
+    name: "Chassis Cab Trucks",
+    logo: require("../assets/images/Chassis Cab Trucks.png"),
+  },
+  {
+    name: "Refrigerated Trucks",
+    logo: require("../assets/images/Refrigerated Trucks.png"),
+  },
+  { name: "Crane Trucks", logo: require("../assets/images/Crane Trucks.png") },
+  {
+    name: "Concrete Trucks",
+    logo: require("../assets/images/Concrete Trucks.png"),
+  },
+  { name: "Vans", logo: require("../assets/images/Vans.png") },
+  { name: "Bus", logo: require("../assets/images/Bus.png") },
+  {
+    name: "Curtain Side Trailers",
+    logo: require("../assets/images/Curtain Side Trailers.png"),
+  },
+  { name: "Trailers", logo: require("../assets/images/Trailers.png") },
+];
