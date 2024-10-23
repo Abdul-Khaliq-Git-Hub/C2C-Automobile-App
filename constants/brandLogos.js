41b5b37f7f930a404fe115e4f592a5f3785e41f0
Abdul Khaliq <abdulkhaliqise2023@gmail.com>
2024-10-24 00:08:45 +0530

    Added brand logos and types used in  search functionality

---
+++ constants/brandLogos.js
@@ -0,0 +1,21 @@
+export default [
+  { name: "Volkswagen", logo: require("../assets/images/Volkswagen.png") },
+  { name: "Scania", logo: require("../assets/images/Scania.png") },
+  { name: "Volvo", logo: require("../assets/images/Volvo.png") },
+  {
+    name: "Schmitz Cargobull",
+    logo: require("../assets/images/Schmitz Cargobull.png"),
+  },
+  {
+    name: "Renault Trucks",
+    logo: require("../assets/images/Renault Trucks.png"),
+  },
+  { name: "Sommer", logo: require("../assets/images/Sommer.png") },
+  { name: "Krone", logo: require("../assets/images/Krone.png") },
+  { name: "Daf", logo: require("../assets/images/Daf.png") },
+  {
+    name: "Mercedes-Benz",
+    logo: require("../assets/images/Mercedes-Benz.png"),
+  },
+  { name: "Iveco", logo: require("../assets/images/Iveco.png") },
+];
