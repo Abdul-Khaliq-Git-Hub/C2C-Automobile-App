ef8b33a4366a3411fc374655736b51ae2d0bb9c4
Abdul Khaliq <abdulkhaliqise2023@gmail.com>
2024-10-30 02:15:50 +0530

    Implemented loading bar and added functionality to navigate to the selected product

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, ActivityIndicator } from "react-native";
+import { View, ScrollView, TouchableOpacity } from "react-native";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
+import { useRouter } from "expo-router";
 import Product from "./Product";
+import * as Progress from "react-native-progress";
+import { useTranslation } from "react-i18next";
 
 type Car = {
   id: string;
   Brand: string;
-  EngineType: string;
-  CylinderType: string;
-  KilometerDriven: string;
-  Location: string;
+  Engine: string;
+  Miles: string;
+  SellerLocation: string;
   Price: string;
   images: string[];
 };
@@ -18,7 +20,8 @@ type Car = {
 const Listings = () => {
   const [cars, setCars] = useState<Car[]>([]);
   const [loading, setLoading] = useState(true);
-
+  const router = useRouter(); // Access the router
+  const { t } = useTranslation();
   const fetchCarsData = async () => {
     try {
       const querySnapshot = await getDocs(collection(db, "TruckListings"));
@@ -41,22 +44,45 @@ const Listings = () => {
   }, []); // Empty dependency array to prevent infinite loop
 
   if (loading) {
-    return <ActivityIndicator size="large" color="#d45e40" />;
+    return (
+      <View style={{ flex: 1 }}>
+        {/* Full width Progress Bar */}
+        <Progress.Bar
+          indeterminate={true}
+          color="#d45e40"
+          width={null} // Allows the bar to take full width
+          borderWidth={0}
+          height={5}
+          animationType="spring"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0, // Ensure it stretches to both sides
+          }}
+        />
+        {/* Additional loading UI can be added here if needed */}
+      </View>
+    );
   }
-  console.log("Render");
+  console.log("listingspage");
   return (
     <ScrollView>
       {cars.map((car) => (
-        <Product
+        <TouchableOpacity
           key={car.id}
-          Brand={car.Brand}
-          Engine={car.EngineType}
-          Cylinder={car.CylinderType}
-          Miles={car.KilometerDriven}
-          Location={car.Location}
-          Price={car.Price}
-          images={car.images}
-        />
+          onPress={() => router.push(`/product-details/${car.id}`)} // Navigate to car details page
+        >
+          <Product
+            id={car.id}
+            Brand={car.Brand}
+            Engine={car.Engine}
+            Miles={car.Miles}
+            Location={car.SellerLocation}
+            Price={car.Price}
+            images={car.images}
+          />
+        </TouchableOpacity>
       ))}
     </ScrollView>
   );
