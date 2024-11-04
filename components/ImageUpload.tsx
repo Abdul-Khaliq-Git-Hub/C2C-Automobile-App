3bf52772294cd0433c726982c8a4d8e8ef926ff7
Abdul Khaliq <abdulkhaliqise2023@gmail.com>
2024-11-05 02:27:30 +0530

    Updated design of Image Uploader

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import { View, Text, TouchableOpacity, ScrollView, Image } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { icons } from "../constants";
+import { useTranslation } from "react-i18next";
 
-const ImageUpload = () => {
-  const [images, setImages] = useState(Array(5).fill(null)); // Array of 4 null values initially
+type ImageUploadProps = {
+  images: (string | null)[];
+  setImages: React.Dispatch<React.SetStateAction<(string | null)[]>>;
+};
 
+const ImageUpload: React.FC<ImageUploadProps> = ({ images, setImages }) => {
   const pickImage = async (index: any) => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -22,18 +26,20 @@ const ImageUpload = () => {
       });
     }
   };
-
+  const { t } = useTranslation();
   return (
     <View className="mt-4 p-2">
       <TouchableOpacity
         onPress={() => pickImage(images.indexOf(null))}
         className="bg-primary h-14 p-4 rounded-xl items-center justify-center"
       >
-        <Text className="text-white text-center">Select Truck Images</Text>
+        <Text className="text-white text-center">
+          {t("Select Truck Images")}
+        </Text>
       </TouchableOpacity>
 
       <View>
-        <Text className="m-4 text-base">Selected Photos</Text>
+        <Text className="m-4 text-base">{t("Selected Photos")}</Text>
         <ScrollView horizontal showsHorizontalScrollIndicator={false}>
           <View className="flex-row space-x-4 px-4">
             {images.map((image, index) => (
@@ -61,7 +67,7 @@ const ImageUpload = () => {
                 </View>
                 {index === 0 && (
                   <Text className="text-white text-black text-lg text-center mt-2">
-                    Cover
+                    {t("Cover")}
                   </Text>
                 )}
               </TouchableOpacity>
